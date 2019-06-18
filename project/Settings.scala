4cb3e2155ebc47d6da733003d82e2cfc6539f1fc
Harshad Deo <harshad@simianquant.com>
2019-06-18 19:39:53 +0530

    bump version to 0.1.1-SNAPSHOT

--- project/Settings.scala
+++ project/Settings.scala
@@ -2,7 +2,7 @@ object Settings {
   object versions {
     val asm: String = "6.0"
     val macroParadise: String = "2.1.0"
-    val project: String = "0.1.0"
+    val project: String = "0.1.1-SNAPSHOT"
     val scala: String = "2.12.6"
   }
 }
